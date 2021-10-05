f502fee8c9cf203debe39598e3a2b3183c6313b0
ChrsitnaSVTH <christina.sisavath@gmail.com>
2021-10-05 10:50:13 +0200

    essaie hamburger

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const headertoggleButton = document.getElementById('headerToggleButton');
+gsap.from(".headerlogo", {duration: 1, scale: 6});
+gsap.to(".headerlogo", {duration: 2, rotation: 360, scale: 5});
+
+
+// responsive hamburger 
+
+
+const headerToggleButton = document.getElementById('headerToggleButton');
 const headerResponsiveBarre = document.getElementById('headerResponsiveBarre');
 
 headerToggleButton.addEventListener('click', show);
@@ -6,6 +13,3 @@ headerToggleButton.addEventListener('click', show);
 function show(){
     headerResponsiveBarre.classList.toggle('active');
 }
-
-gsap.from(".headerlogo", {duration: 1, scale: 6});
-gsap.to(".headerlogo", {duration: 2, rotation: 360, scale: 5});
